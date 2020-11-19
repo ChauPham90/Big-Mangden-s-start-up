0e612ae21907ec951ae352b326d8bd206ba52aa1
ChauPham90 <minhchaupham90@gmail.com>
2020-11-19 17:12:20 +0700

    Updates

---
+++ precache-manifest.d4b6fdb4951e88d51d5f404a0cf8d993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f41e8cd5d5a60addf86d4ea021b2793",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e79d86de584822d8ab10",
+    "url": "/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "e79d86de584822d8ab10",
+    "url": "/static/js/2.6e31f0e0.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.6e31f0e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a395cbe487b99f5063",
+    "url": "/static/js/main.50016908.chunk.js"
+  },
+  {
+    "revision": "0f84405c20dec1ac45e7",
+    "url": "/static/js/runtime-main.3faf3100.js"
+  }
+]);
