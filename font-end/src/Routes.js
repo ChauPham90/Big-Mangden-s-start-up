0476c37995fdd58a5c605ac872b06681915b6d71
ChauPham90 <minhchaupham90@gmail.com>
2020-11-19 11:47:09 +0700

    add userDashboard branch and uncompleted dashBoard feature

--- font-end/src/Routes.js
+++ font-end/src/Routes.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
+import Dashboard from "./user/UserDashboard";
+import PrivateRoute from "./state/privateRoute";
+
 import Home from "./core/Home";
 
 const Routes = () => {
@@ -11,6 +14,11 @@ const Routes = () => {
         <Route path="/signup" exact component={Signup}></Route>
         <Route path="/signin" exact component={Signin}></Route>
         <Route path="/" exact component={Home}></Route>
+        <PrivateRoute
+          path="/dashboard"
+          exact
+          component={Dashboard}
+        ></PrivateRoute>
       </Switch>
     </BrowserRouter>
   );
