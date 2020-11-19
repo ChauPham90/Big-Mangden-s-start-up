0b079d272a45f85c85bf676200fa4afa2ccf0f91
ChauPham90 <minhchaupham90@gmail.com>
2020-11-19 17:08:48 +0700

    add github config to deploy

--- font-end/src/App.js
+++ font-end/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App">hello from Big Mang Den</div>;
+  return <div className="App">hello from Big Mang Den.</div>;
 }
 
 export default App;
